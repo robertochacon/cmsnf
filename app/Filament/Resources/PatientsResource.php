32edc279e526aa1d27ff6ade2685de0044382866
rchaconalcantara <hola.hack09@gmail.com>
2023-11-24 19:37:46 -0400

    Changes - improvements in patients

--- app/Filament/Resources/PatientsResource.php
+++ app/Filament/Resources/PatientsResource.php
@@ -6,12 +6,19 @@ use App\Filament\Resources\PatientsResource\Pages;
 use App\Filament\Resources\PatientsResource\RelationManagers;
 use App\Models\Patients;
 use Filament\Forms;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
 class PatientsResource extends Resource
 {
@@ -25,7 +32,23 @@ class PatientsResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Section::make()
+                ->schema([
+                    TextInput::make('institution')->required(),
+                    TextInput::make('identification')->required(),
+                    TextInput::make('name')->required(),
+                    TextInput::make('rango')->required(),
+                    TextInput::make('phone')->numeric(),
+                    TextInput::make('age')->numeric(),
+                    TextInput::make('sexo')->numeric(),
+                    Toggle::make('status')
+                ])
+                ->columns(3),
+                Section::make()
+                ->schema([
+                    Textarea::make('address'),
+                ])
+                ->columns(2)
             ]);
     }
 
@@ -33,16 +56,26 @@ class PatientsResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('identification')
+                ->searchable(),
+                TextColumn::make('name')
+                ->searchable(),
+                TextColumn::make('rango')->default('N/A'),
+                TextColumn::make('age')->default('N/A'),
+                TextColumn::make('phone')->default('N/A'),
+                TextColumn::make('created_at')->since(),
+                ToggleColumn::make('status')
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
+                    ExportBulkAction::make(),
                     Tables\Actions\DeleteBulkAction::make(),
                 ]),
             ]);
