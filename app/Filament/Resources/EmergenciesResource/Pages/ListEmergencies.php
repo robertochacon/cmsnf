35702f1484b0c35d009d42fc03c3f8a953121164
rchaconalcantara <hola.hack09@gmail.com>
2023-11-26 21:35:43 -0400

    Changes - improvments in emergencies

--- app/Filament/Resources/EmergenciesResource/Pages/ListEmergencies.php
+++ app/Filament/Resources/EmergenciesResource/Pages/ListEmergencies.php
@@ -3,8 +3,11 @@
 namespace App\Filament\Resources\EmergenciesResource\Pages;
 
 use App\Filament\Resources\EmergenciesResource;
+use App\Models\Emergencies;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListEmergencies extends ListRecords
 {
@@ -14,6 +17,23 @@ class ListEmergencies extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'Todos' => Tab::make()
+                ->badge(Emergencies::query()->count()),
+            'Atendiendo' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'Atendiendo'))
+                ->badge(Emergencies::query()->where('status', 'Atendiendo')->count()),
+            'Atendidas' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'Atendida'))
+                ->badge(Emergencies::query()->where('status', 'Atendida')->count()),
+            'Traslado' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query) => $query->where('status', 'Traslado'))
+                ->badge(Emergencies::query()->where('status', 'Traslado')->count()),
         ];
     }
 }
