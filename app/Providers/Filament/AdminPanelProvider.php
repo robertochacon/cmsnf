f5b6d4b2f00a420d58ef2d132cad7616363e91a0
rchaconalcantara <hola.hack09@gmail.com>
2024-04-29 17:44:59 -0400

    Changes - improvements in styles

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -68,6 +68,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->viteTheme('resources/css/filament/admin/theme.css');
     }
 }
