9ded7316f4deb218b053ec72de7545c777e4de11
rchaconalcantara <hola.hack09@gmail.com>
2024-08-23 11:37:35 -0400

    Ocultando filtro del dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -50,7 +50,7 @@ class AdminPanelProvider extends PanelProvider
                             ->directory('images/backgrounds')
                     ),
             ])
-            ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
+            // ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 // Widgets\AccountWidget::class,
             ])
