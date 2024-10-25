13019d54f1c9e86dab207085acd775a7070f4e0a
rchaconalcantara <hola.hack09@gmail.com>
2024-10-25 14:19:38 -0400

    Build de los estilos

--- resources/css/filament/admin/tailwind.config.js
+++ resources/css/filament/admin/tailwind.config.js
@@ -6,5 +6,7 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './resources/css/**/*.css',
+        './resources/views/**/*.blade.php',
     ],
 }
